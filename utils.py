caa8a945ffbd45d813999af8c38fc67606602aa6
Karim Djemai <karimdjemai1997@gmail.com>
2024-08-28 22:16:57 +0200

    add sort function

---
+++ utils.py
@@ -0,0 +1,5 @@
+def compare_player_names(search_string, parameter_string):
+    str1 = str1 + ' ' * (len(str2) - len(str1))
+        str2 = str2 + ' ' * (len(str1) - len(str2))
+        return sum(1 if i == j else 0
+                for i, j in zip(str1, str2)) / float(len(str1))
